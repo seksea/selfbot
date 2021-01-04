b6fdecf22c36b01ffb38eefeaa377bfb5b9d70fc
sekc <sekc@sekc.me>
2021-01-04 13:47:16 +0000

    first commit!

---
+++ readme.md
@@ -0,0 +1,27 @@
+<p align="center">
+  <a><img src="https://img.shields.io/badge/Discord-gray.svg" alt="Discord"></a>
+  <a><img src="https://img.shields.io/badge/Language-Python3.7+-blue.svg" alt="Python3.7+"></a>
+  <a><img src="https://img.shields.io/badge/State-Unfinished-red.svg" alt="Unfinished"></a>
+  <p align="center"><img src="2020-11-18-153742_212x201_scrot.png"></p>
+</p>
+
+:warning: Although the risk is low, using a selfbot in discord is against the terms of service and your account *could* be banned, although its highly unlikely from past experience :warning:
+
+A python discord selfbot
+
+## How to get your token
+This video showcases [how to get your token](https://www.youtube.com/watch?v=YEgFvgg7ZPI)
+
+## How to use
+```
+git clone https://github.com/seksea/auto-status
+cd selfbot
+pip install -r requirements.txt
+vim config.json # Use any text editor and edit to your liking
+python3 selfbot.py
+```
+
+## Found a bug/want to add something?
+- [Create a pull request](https://github.com/seksea/selfbot/issues/new) ([tutorial](https://github.com/yangsu/pull-request-tutorial))
+- [Report a bug](https://github.com/seksea/selfbot/issues/new)
+
---
+++ selfbot.py
@@ -0,0 +1,58 @@
+import discord
+import json
+import datetime
+import asyncio
+
+dClient = discord.Client()
+
+# Load config
+config = {}
+with open("config.json", "r") as f:
+    config = json.loads(f.read())
+
+async def statusTask():
+    while True:
+        for status in config["statuses"]:
+            if status["type"] == "game":
+                await dClient.change_presence(activity=discord.Game(name=status["name"], start=datetime.datetime.now()-datetime.timedelta(status["duration"]), end=datetime.datetime.now()))
+            if status["type"] == "stream":
+                await dClient.change_presence(activity=discord.Streaming(name=status["name"], platform="YouTube", url=status["url"]))
+            if status["type"] == "competing":
+                await dClient.change_presence(activity=discord.Activity(name=status["name"], type=5, details=status["description"]))
+            if status["type"] == "watching":
+                await dClient.change_presence(activity=discord.Activity(name=status["name"], type=3, details=status["description"]))
+            await asyncio.sleep(status["delay"])
+
+@dClient.event
+async def on_message(message : discord.message):
+    if message.author == dClient.user:
+        if message.content.startswith(config["config"]["prefix"]):
+            command = message.content[1:].split(" ")[0].lower()
+            args = message.content[len(message.content.split(" ")[0])+1:]
+            
+            if command == "showchannels":
+                guild = dClient.get_guild(int(args))
+                embed=discord.Embed(title=" ", color=int(config["embeds"]["success color"], 0))
+                embed.set_author(name=f"Channels in {guild.name}", icon_url=guild.icon_url)
+                embed.set_footer(text=config["embeds"]["footer"])
+                for channel in guild.text_channels:
+                    try:
+                        lastmsg = await channel.history(limit=1).next()
+                        embed.add_field(name=channel.name, value=":keyboard:", inline=False)
+                    except discord.NoMoreItems:
+                        embed.add_field(name=channel.name, value=":keyboard:", inline=False)
+                    except:
+                        embed.add_field(name=channel.name, value=":keyboard: :no_entry_sign:", inline=False)
+                for channel in guild.voice_channels:
+                    embed.add_field(name=channel.name, value=":speaker:", inline=False)
+                await message.channel.send("", embed=embed)
+            
+            await message.delete()
+
+
+@dClient.event
+async def on_ready():
+    print(f"logged in as {dClient.user} and changing status!")
+    dClient.loop.create_task(statusTask())
+
+dClient.run(config["config"]["token"], bot=False)
